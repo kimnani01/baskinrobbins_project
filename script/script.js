5d612c109c0de2cde70b40b3f4bd5733682bcc26
kimnani01 <165613415+kimnani01@users.noreply.github.com>
2024-07-01 16:43:11 +0900

    메뉴-음료페이지

--- script/script.js
+++ script/script.js
@@ -1,13 +1,34 @@
-const event = new Swiper('.event', {
-        slidesPerView: 5,
-        slidesPerGroup: 5,
-        spaceBetween: 40,
-        speed: 3000,
-        autoplay:{delay:3000},
-        loop: true,
-        pagination:{
-            el:'.event .swiper-pagination', //부모 필수
-            type:'bullets',
-            clickable: true,
-        }   
-})
+const gnb = document.querySelectorAll('.gnb > li')
+for(let i of gnb){
+    i.addEventListener('mouseover', ()=>{
+        console.log(i)
+        i.lastElementChild.style.display = 'flex'
+        i.firstChild.style.color = '#000'
+    })
+    i.addEventListener('mouseout', ()=>{
+        i.lastElementChild.style.display = 'none'
+        i.firstChild.style.color = '#fff'
+    })
+}
+const sub = document.querySelectorAll('.sub > li')
+const depth1 = document.querySelectorAll('.depth1 > li > a')
+for(let i of sub){
+    i.addEventListener('mouseover', ()=>{
+        console.log(i)
+        i.firstElementChild.style.color = '#ff4d72'
+    })
+    i.addEventListener('mouseout', ()=>{
+        console.log(i)
+        i.firstElementChild.style.color = '#000'
+    })
+}
+for(let j of depth1){
+    j.addEventListener('mouseover', ()=>{
+        j.style.color = '#ff4d72'
+        j.parentElement.parentElement.previousElementSibling.style.color = '#ff4d72'
+    })
+    j.addEventListener('mouseout', ()=>{
+        j.style.color = '#000'
+        j.parentElement.parentElement.previousElementSibling.style.color = '#000'
+    })
+}
